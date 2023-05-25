78dd0773c4ec7573bc1cd48506d3c2f48d6466e9
JuanL12 <126787451+JuanL12@users.noreply.github.com>
2023-05-25 08:29:34 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220, 220, 220)
+}
+
+function draw() {
+  fill(255, 255, 255, 255)
+  ellipse(200, 310, 200, 200)
+  fill(255, 255, 255);
+  ellipse(200, 150, 165, 165);
+  fill(0, 0, 0)
+  ellipse(165, 140, 50, 50)
+  fill(0, 0, 0)
+  ellipse(235, 140, 50, 50)
+}
+
